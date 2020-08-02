1afcba77c047ebf3ab24e91bb532563b6efbb654
ahmed <ahmedsh717@gmail.com>
2020-08-02 22:21:42 +0300

    CONNECT

---
+++ server.js
@@ -0,0 +1,61 @@
+'use strict';
+require('dotenv').config()
+const express = require('express');
+const sql = require('mssql');
+const server = express();
+const PORT = process.env.PORT || 3030;
+const config = {
+	server: process.env.SERVER,
+	authentication: {
+		type: 'default',
+		options: {
+			userName: process.env.USER_NAME,
+			password: process.env.PASS,
+		},
+	},
+	options: {
+		database: 'sample',
+		encrypt: false,
+	}
+}
+server.get('/add',(req,res)=>{
+    var conn = new sql.ConnectionPool(config);
+    var requis = new sql.Request(conn);
+    conn.connect(error=>{
+        if (error){
+            console.log(error);
+            return;
+        }
+        requis.query(`insert into [sample].[dbo].[users] values ('ahmad','123')`,(err,data)=>{
+            if(err){
+                console.log(err);
+            }else{
+                res.status(200).send('Done');
+            }
+            conn.close();
+        })
+    })
+});
+server.get('/',(req,res)=>{
+        var conn = new sql.ConnectionPool(config);
+    var requis = new sql.Request(conn);
+    conn.connect(error=>{
+        if (error){
+            console.log(error);
+            return;
+        }
+        requis.query(`select * from [sample].[dbo].[users] `,(err,data)=>{
+            if(err){
+                console.log(err);
+            }else{
+                res.status(200).send(data.recordset);
+            }
+            conn.close();
+        })
+    })
+    // res.send('hi');
+})
+
+server.listen(PORT,()=>{
+    console.log(`listen to port ${PORT}`);
+})
