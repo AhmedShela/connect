54d414354f453fc5a79d582eda209f969ace7eee
ahmed <ahmedsh717@gmail.com>
2020-08-03 14:52:21 +0300

    asure

--- server.js
+++ server.js
@@ -1,33 +1,33 @@
-const { Connection, Request } = require("tedious");
+const {Connection, Request} = require("tedious");
 require('dotenv').config()
 const express = require('express');
 const server = express();
 const PORT = process.env.PORT || 3030;
 // Create connection to database
 const config = {
-  authentication: {
-    options: {
-      userName: process.env.USER_NAME, // update me
-      password: process.env.PASS // update me
+    authentication: {
+        options: {
+            userName: "KHAL", // update me
+            password: "Theshadow1993!@#" // update me
+        },
+        type: "default"
     },
-    type: "default"
-  },
-  server: process.env.SERVER, // update me
-  options: {
-    database: "HR_Data", //update me
-    encrypt: true
-  }
+    server: "khal-server.database.windows.net", // update me
+    options: {
+        database: "HR_Data", // update me
+        encrypt: true
+    }
 };
 
 const connection = new Connection(config);
 
 // Attempt to connect and execute queries if connection goes through
 connection.on("connect", err => {
-  if (err) {
-    console.error(err.message);
-  } else {
-    // queryDatabase();
-  }
+    if (err) {
+        console.error(err.message);
+    } else {
+        // queryDatabase();
+    }
 });
 server.get('/',(req,res)=>{
     console.log("Reading rows from the Table...");
@@ -39,47 +39,48 @@ server.get('/',(req,res)=>{
         if (err) {
           console.error(err.message);
         } else {
-            
+
           console.log(`${rowCount} row(s) returned`);
         }
       }
     );
-  
-    request.on("row", columns => {
-     let data = columns.map(column => {
-        return column.metadata.colName, column.value;
+
+    request.on("requestCompleted", columns => {
+        // let arr = columns.map(column =>{
+        //     return column.value
+        // })
+        res.send(columns)
+        // columns.forEach(column => {
+        //   console.log("%s\t%s", column.metadata.colName, column.value);
+        // });
       });
-      res.send(data)
-    });
-  
+
     connection.execSql(request);
 });
 
-server.listen(PORT,()=>{
+// function queryDatabase(req,res) {
+//     console.log("Reading rows from the Table...");
+
+//     // Read all rows from table
+//     const request = new Request(`select * from [HR_Data].[dbo].[Emp_general]`, (err, rowCount) => {
+//         if (err) {
+//             console.error(err.message);
+//         } else {
+
+//             console.log(`${rowCount} row(s) returned`);
+//         }
+//     });
+
+//     request.on("row", columns => {
+//         let data = columns.map(column => {
+//             return column.metadata.colName,
+//             column.value;
+//         });
+//         res.send(data)
+//     });
+
+//     connection.execSql(request);
+// }
+server.listen(PORT, () => {
     console.log(`listen to port ${PORT}`);
 })
-// function queryDatabase(req,res) {
-//   console.log("Reading rows from the Table...");
-
-//   // Read all rows from table
-//   const request = new Request(
-//     `select * from [HR_Data].[dbo].[Emp_general]`,
-//     (err, rowCount) => {
-//       if (err) {
-//         console.error(err.message);
-//       } else {
-          
-//         console.log(`${rowCount} row(s) returned`);
-//       }
-//     }
-//   );
-
-//   request.on("row", columns => {
-//    let data = columns.map(column => {
-//       return column.metadata.colName, column.value;
-//     });
-//     res.send(data)
-//   });
-
-//   connection.execSql(request);
-// }
